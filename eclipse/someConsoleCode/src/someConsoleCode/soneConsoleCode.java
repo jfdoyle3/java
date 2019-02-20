d8b8bb2b0637d7939fc96dc489ee851bfcd8b5e1
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2019-02-20 15:53:42 -0500

    Eclispe: added area of triangle
    Working in Eclipse

---
+++ eclipse/someConsoleCode/src/someConsoleCode/soneConsoleCode.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package someConsoleCode;
+
+/**
+ * @author Jim
+ *	running through some basic Java code
+ *	using eclipse
+ */
+public class soneConsoleCode {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+//   If Statement		
+		int number=24;
+			
+			if (number==24) {
+				System.out.println("Number is 24");
+			} else {
+				System.out.println("Number is not 24");
+			}
+	
+
+	}
+
+}
