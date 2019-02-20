24a6dd13dcc1868384483aabb58ce06d6930ca62
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2019-02-20 10:24:54 -0500

    added a simple console program

---
+++ resistance.java
@@ -0,0 +1,12 @@
+// This is a comment and
+/*
+ *
+ * this is also a comment
+ *
+ */
+
+public class Resistance {
+    public static void main(String[] args) {
+        system.out.println("Resistance is Futile!!");
+    }
+}
