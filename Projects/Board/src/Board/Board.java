aacf067ffc3d2d6d5ecf28d53fffa150143dcd03
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2019-03-01 09:13:11 -0500

    got a simple character grid working

--- Projects/Board/src/Board/Board.java
+++ Projects/Board/src/Board/Board.java
@@ -19,15 +19,31 @@ public class Board {
 
 	public static void main(String[] args) {
 	
-	Grid squares=new Grid();
-	
-	
+		int x=5;
+		int y=5;
+//		String corner="+";
+//		String hEdge="-";
+//		String vEdge="|";
+//		String space=" ";
+//		
+//		System.out.print(corner);
+		for (int i=1; i<=x;i++)
+		{
+		  for (int j=1; j<=y; j++) {
+			if (i == j) {
+				System.out.print("1 ");
+			}else {
+				System.out.print("0 ");
+			}
+		}
+		  System.out.print("\n");
+		}
+  }
+}
+
 //	input = new Scanner(System.in);
 //	System.out.println("How many spaces across?");
 //	int x=input.nextInt();
 //	System.out.println("How many spaces Down");
 //	int y=input.nextInt();
 	
-	}
-	
-}
