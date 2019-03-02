18782b101e94d21ac32bdb450c462d003106e930
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2019-03-02 08:13:53 -0500

    created a test file to test code

---
+++ Test/src/Test/Test.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package Test;
+
+/**
+ * @author Jim Doyle
+ *
+ */
+public class Test {
+
+	/**
+	 * Test Code
+	 */
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 4; i++) {
+		    for (int j = 0; j < i; j++) {
+		        System.out.print(j);
+		    }
+		    System.out.println();
+		}
+
+	}
+
+}
