bb9a6e511b63913f70bb346cab0350004803d7cc
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2019-03-21 15:50:56 -0400

    move code to folders

--- codeExamples/bp/arraying/src/arraying/arraying.java
+++ codeExamples/bp/arraying/src/arraying/arraying.java
@@ -16,9 +16,9 @@ public class arraying {
 		
 		//int arr={2,5,6,77,11};
 		int arr[]= new int[5];
-		for (int i=0; i<=4; i++) {
-		arr[i]=i+1;
-		System.out.println(arr[i]);
-		}
+		//for (int i=0; i<=4; i++) {
+		//arr[i]=i+1;
+		System.out.println(arr);
+		//}
 	}
 }
